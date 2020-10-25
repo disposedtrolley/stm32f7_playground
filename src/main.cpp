72148f7edb7b9121a7d9ab29106782abf760d18a
James Liu <james_liu@fastmail.com>
2020-10-25 14:30:46 +1100

    refactor: clean up logic

--- src/main.cpp
+++ src/main.cpp
@@ -3,71 +3,73 @@
 #include <drivers/gpio.h>
 #include <zephyr.h>
 
-static const struct adc_channel_cfg adc_channel_9_cfg = {
-    .gain = ADC_GAIN_1,
-    .reference = ADC_REF_INTERNAL,
-    .channel_id = 9
+struct ADCChannel {
+  uint8_t id;
+  uint8_t resolution;
+  uint16_t buffer[1];
 };
-
-static const struct adc_channel_cfg adc_channel_12_cfg = {
-    .gain = ADC_GAIN_1,
-    .reference = ADC_REF_INTERNAL,
-    .channel_id = 12
-};
-
-static uint16_t m_sample_buffer[1];
-static uint16_t m_sample_buffer2[1];
 
 void main() {
   // LD1 is connected to PB0.
   const struct device *pb = device_get_binding("GPIOB");
   gpio_pin_configure(pb, 0, GPIO_OUTPUT);
 
-  const struct device *adc = device_get_binding("ADC_1");
-  if (adc == nullptr) {
+  // Configure the ADC channels we're interested in.
+  ADCChannel const channels[] = {
+    {
+      .id = 9,
+      .resolution = 12,
+      .buffer = {},
+    },
+    {
+      .id = 12,
+      .resolution = 12,
+      .buffer = {},
+    }
+  };
+
+  // Get ADC1.
+  const struct device *adc_dev = device_get_binding("ADC_1");
+  if (adc_dev == nullptr) {
     printk("failed to get ADC_1\n");
+    return;
   }
 
-  int ret = adc_channel_setup(adc, &adc_channel_9_cfg);
-  if (ret != 0) {
-    printk("failed to setup ADC1_IN9: %d\n", ret);
+  // Setup channels for ADC1.
+  for (ADCChannel chan : channels) {
+    struct adc_channel_cfg adc_cfg = {
+      .gain = ADC_GAIN_1,
+      .reference = ADC_REF_INTERNAL,
+      .channel_id = chan.id,
+    };
+    int err = adc_channel_setup(adc_dev, &adc_cfg);
+    if (err != 0) {
+      printk("adc_channel_setup() :: failed to setup channel %d: %d\n", chan.id, err);
+      return;
+    }
   }
-
-  ret = adc_channel_setup(adc, &adc_channel_12_cfg);
-  if (ret != 0) {
-    printk("failed to setup ADC1_IN12: %d\n", ret);
-  }
-
-  const struct adc_sequence sequence_chan_9 = {
-      .channels = BIT(9),
-      .buffer = m_sample_buffer,
-      .buffer_size = sizeof(m_sample_buffer),
-      .resolution = 12,
-  };
-
-  const struct adc_sequence sequence_chan_12 = {
-      .channels = BIT(12),
-      .buffer = m_sample_buffer2,
-      .buffer_size = sizeof(m_sample_buffer2),
-      .resolution = 12,
-  };
-
 
   while (true) {
-    int ret = adc_read(adc, &sequence_chan_9);
-    if (ret != 0) {
-      printk("adc_read() failed with code %d", ret);
-    } else {
-      printk("channel 9: %d\n", m_sample_buffer[0]);
-    }
+    // Read each channel consecutively. The STM32 ADC driver doesn't support
+    // reading from multiple channels in the same sequence struct:
+    // https://github.com/zephyrproject-rtos/zephyr/pull/13447#issuecomment-464420196
+    for (ADCChannel chan : channels) {
+      struct adc_sequence seq_cfg = {
+        .channels = BIT(chan.id),
+        .buffer = chan.buffer,
+        .buffer_size = sizeof(chan.buffer),
+        .resolution = chan.resolution,
+      };
 
-    ret = adc_read(adc, &sequence_chan_12);
-    if (ret != 0) {
-      printk("adc_read() failed with code %d", ret);
-    } else {
-      printk("channel 12: %d\n", m_sample_buffer2[0]);
+      int ret = adc_read(adc_dev, &seq_cfg);
+      if (ret != 0) {
+        printk("adc_read() :: failed with code %d", ret);
+        return;
+      } 
+      
+      printk("adc_read() :: channel %d: %d\n", chan.id, chan.buffer[0]);
     }
-
+    
     // Blinky
     gpio_pin_toggle(pb, 0);
     k_msleep(500);
